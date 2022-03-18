827b8ee7b4427c973294b019df82bb1cb80df31b
Eshwari Bhide <ebhide99@gmail.com>
2022-03-17 21:42:06 -0700

    Basic draft for sticker messaging

--- app/src/main/java/edu/neu/madcourse/numad22sp_teamdedj/StickerMessagingActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_teamdedj/StickerMessagingActivity.java
@@ -2,13 +2,134 @@ package edu.neu.madcourse.numad22sp_teamdedj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 
 public class StickerMessagingActivity extends AppCompatActivity {
+    private static final String TAG = "StickerMessagingActivity";
+
+    private static final String SERVER_KEY="key=AAAAP4z9QU0:APA91bECheSrt__KSX5dPa-DfGEfb_fWzgi3_E38lvWsyyHenK9F05Uqfo4bjPXhjKjQCXBt5CgtvpC09PQ4c4oZDaHC8ZLHRTBXveiLzQQ5YWDFg9t3Qfod4AKGVMccnQTzxMaQhFWV";
+
+    // This is the client registration token
+    private static String CLIENT_REGISTRATION_TOKEN;
+
+    // Test your msg: https://console.firebase.google.com/project/<your-project-name>/notification/compose
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker_messaging);
+
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Toast toast = Toast.makeText(StickerMessagingActivity.this, "Cannot get token", Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+                if (CLIENT_REGISTRATION_TOKEN == null) {
+                    CLIENT_REGISTRATION_TOKEN = task.getResult();
+                }
+                Log.e("CLIENT_REGISTRATION_TOKEN", CLIENT_REGISTRATION_TOKEN);
+                Toast toast = Toast.makeText(StickerMessagingActivity.this, "CLIENT_REGISTRATION_TOKEN EXISTS", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
+
+    public void sendStickerMessage(View view) {
+
+        new Thread(() -> sendStickerMessage(CLIENT_REGISTRATION_TOKEN)).start();
+    }
+
+    private void sendStickerMessage(String targetToken) {
+
+        JSONObject jPayload = new JSONObject();
+        JSONObject jNotification = new JSONObject();
+        JSONObject jdata = new JSONObject();
+        try {
+            jNotification.put("title", "Message Title from 'SEND MESSAGE TO CLIENT BUTTON'");
+            jNotification.put("body", "Message body from 'SEND MESSAGE TO CLIENT BUTTON'");
+            jNotification.put("sound", "default");
+            jNotification.put("badge", "1");
+
+            jdata.put("title", "data title from 'SEND MESSAGE TO CLIENT BUTTON'");
+            jdata.put("content", "data content from 'SEND MESSAGE TO CLIENT BUTTON'");
+
+            jPayload.put("to", targetToken);
+
+            jPayload.put("priority", "high");
+            jPayload.put("notification", jNotification);
+            jPayload.put("data", jdata);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String resp = "NULL";
+        try {
+            URL url = new URL("https://fcm.googleapis.com/fcm/send");
+            HttpURLConnection req = (HttpURLConnection) url.openConnection();
+            req.setRequestMethod("POST");
+            req.setRequestProperty("Content-Type", "application/json");
+            req.setRequestProperty("Authorization", SERVER_KEY);
+            req.setDoOutput(true);
+
+            OutputStream outputStream = req.getOutputStream();
+            outputStream.write(jPayload.toString().getBytes());
+            outputStream.close();
+
+            Scanner s = new Scanner(req.getInputStream()).useDelimiter("\\A");
+            resp = s.hasNext() ? s.next() : "";
+
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception in sending message");
+        }
+        postToastMessage("Status from Server: " + resp, getApplicationContext());
+
+    }
+
+    public static void postToastMessage(final String message, final Context context){
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(() -> Toast.makeText(context, message, Toast.LENGTH_LONG).show());
+    }
+
+    /* Dive deep into https://medium.com/@selvaganesh93/firebase-cloud-messaging-important-rest-apis-be79260022b5
+     */
+    public static String fcmHttpConnection(String serverToken, JSONObject jsonObject) {
+        try {
+            URL url = new URL("https://fcm.googleapis.com/fcm/send");
+            HttpURLConnection req = (HttpURLConnection) url.openConnection();
+            req.setRequestMethod("POST");
+            req.setRequestProperty("Content-Type", "application/json");
+            req.setRequestProperty("Authorization", serverToken);
+            req.setDoOutput(true);
+
+            OutputStream outputStream = req.getOutputStream();
+            outputStream.write(jsonObject.toString().getBytes());
+            outputStream.close();
+
+            Scanner s = new Scanner(req.getInputStream()).useDelimiter("\\A");
+            return s.hasNext() ? s.next() : "";
+        } catch (IOException e) {
+            return "NULL";
+        }
+
+    }
+    
 }

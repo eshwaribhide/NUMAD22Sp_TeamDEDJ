383c006adcbd65d127531602fe2a4c5f7425ce6f
Eshwari Bhide <ebhide99@gmail.com>
2022-03-17 18:20:34 -0700

    Added button to create new activity for sticker app

--- app/src/main/java/edu/neu/madcourse/numad22sp_teamdedj/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_teamdedj/MainActivity.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.numad22sp_teamdedj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Details are in 'fcm' package
+    public void stickerMessagingActivityOnClick(View view) {
+        Intent intent = new Intent(this, StickerMessagingActivity.class);
+        startActivity(intent);
     }
 }
